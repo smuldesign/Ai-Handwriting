7253419a103e3a1ec8518f2d34f91e73ec6d1615
Rico Zethof <Ricozethof@gmail.com>
2019-09-18 08:52:39 +0200

    Debug

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const uploadFolder = '/uploads/images';
 const upload = multer({dest: __dirname + '/public/uploads/images'});
 const getimage = require('./get-image-url.js');
 
@@ -15,10 +16,9 @@ app.use(express.static('public'));
 app.post('/upload', upload.single('photo'), (req, res) => {
     var homeUrl = req.protocol+"://"+req.headers.host;
     if(req.file) {
-        let url = JSON.stringify(req.file);
+        let name = JSON.stringify(req.file.filename);
+        let url = homeUrl + uploadFolder + '/' + name;
         console.log(url);
-        console.log(homeUrl);
-        console.log(__dirname);
     }
     else throw 'error';
 });
